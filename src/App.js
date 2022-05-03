be7a736688ae4d8d796fa5383297ff34b905234b
keroloussamy <keroloussamy98@gmail.com>
2022-05-03 21:25:12 +0200

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import store from './redux/store';
+import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Greeting } from './components/Greeting';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+      <Route exact path="" element={<Greeting />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
   );
 }
 
